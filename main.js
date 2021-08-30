52e83ac960799ecf75267aa994a92eb79615d823
Danny Benshi <benshidanny11@gmail.com>
2021-08-30 19:04:25 +0200

    Implement required changes

--- main.js
+++ main.js
@@ -2,23 +2,13 @@ document.getElementById("menu-btn").addEventListener("click", function () {
   let menuList = document.getElementById("menu-list");
   menuList.classList.remove("show-menu");
 });
-
-document.getElementById("btn-hide").addEventListener("click", function () {
-  let menuList = document.getElementById("menu-list");
-  menuList.classList.add("show-menu");
+var menus=document.getElementsByClassName("hide-menu");
+var menuarray = Array.prototype.map.call(menus, function(element) {
+  return element;
 });
-
-document.getElementById("mnu-port").addEventListener("click", function () {
-  let menuList = document.getElementById("menu-list");
-  menuList.classList.add("show-menu");
-});
-
-document.getElementById("mnu-about").addEventListener("click", function () {
-  let menuList = document.getElementById("menu-list");
-  menuList.classList.add("show-menu");
-});
-
-document.getElementById("mnu-contact").addEventListener("click", function () {
-  let menuList = document.getElementById("menu-list");
-  menuList.classList.add("show-menu");
+menuarray.forEach((element) => {
+  element.addEventListener("click", function () {
+    let menuList = document.getElementById("menu-list");
+    menuList.classList.add("show-menu");
+  });
 });
